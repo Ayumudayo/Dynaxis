9c59ed5f110d44df2af9e103bfbc581baa6d72b5
Ayumudayo <yumemiayumu@gmail.com>
2026-03-12 00:45:26 +0900

    build: package storage factory targets

--- tests/core/run_installed_factory_package_consumer.cmake
+++ tests/core/run_installed_factory_package_consumer.cmake
@@ -1,13 +1,12 @@
 cmake_minimum_required(VERSION 3.20)
 
-foreach(required_var BUILD_DIR CONSUMER_SOURCE_DIR CONSUMER_BUILD_DIR INSTALL_PREFIX SERVER_CORE_PACKAGE_DIR CONFIG GENERATOR)
+foreach(required_var BUILD_DIR CONSUMER_SOURCE_DIR CONSUMER_BUILD_DIR INSTALL_PREFIX CONFIG GENERATOR)
     if(NOT DEFINED ${required_var} OR "${${required_var}}" STREQUAL "")
-        message(FATAL_ERROR "run_installed_package_consumer.cmake missing required variable: ${required_var}")
+        message(FATAL_ERROR "run_installed_factory_package_consumer.cmake missing required variable: ${required_var}")
     endif()
 endforeach()
 
 file(REMOVE_RECURSE "${CONSUMER_BUILD_DIR}")
-file(REMOVE_RECURSE "${INSTALL_PREFIX}")
 file(MAKE_DIRECTORY "${CONSUMER_BUILD_DIR}")
 
 execute_process(
@@ -17,7 +16,7 @@ execute_process(
     ERROR_VARIABLE install_stderr
 )
 if(NOT install_result EQUAL 0)
-    message(FATAL_ERROR "server_core install failed\n${install_stdout}\n${install_stderr}")
+    message(FATAL_ERROR "factory package install failed\n${install_stdout}\n${install_stderr}")
 endif()
 
 set(configure_cmd
@@ -26,7 +25,6 @@ set(configure_cmd
     -B "${CONSUMER_BUILD_DIR}"
     -G "${GENERATOR}"
     "-DCMAKE_PREFIX_PATH=${INSTALL_PREFIX}"
-    "-Dserver_core_DIR=${SERVER_CORE_PACKAGE_DIR}"
     "-DCMAKE_BUILD_TYPE=${CONFIG}"
 )
 
@@ -50,7 +48,7 @@ execute_process(
     ERROR_VARIABLE configure_stderr
 )
 if(NOT configure_result EQUAL 0)
-    message(FATAL_ERROR "installed consumer configure failed\n${configure_stdout}\n${configure_stderr}")
+    message(FATAL_ERROR "factory package consumer configure failed\n${configure_stdout}\n${configure_stderr}")
 endif()
 
 execute_process(
@@ -60,5 +58,5 @@ execute_process(
     ERROR_VARIABLE build_stderr
 )
 if(NOT build_result EQUAL 0)
-    message(FATAL_ERROR "installed consumer build failed\n${build_stdout}\n${build_stderr}")
+    message(FATAL_ERROR "factory package consumer build failed\n${build_stdout}\n${build_stderr}")
 endif()
